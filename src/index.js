94089a2886a68cfc1802e143a38fc7fc07459fda
Dhruv Gupta <dhruvgupta828@gmail.com>
2023-09-19 14:56:43 +0530

    project done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import { Box } from '@mui/system';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import "./index.css";
 
@@ -9,6 +8,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <App /> 
-
   </>
 );
